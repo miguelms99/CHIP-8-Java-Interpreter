a53a1ccf5c110e5e474ede7734c4ae528e9b91b0
Miguel Moreno <miguelms@tutanota.com>
2021-03-30 21:34:53 +0200

    Some refactoring

--- src/main/java/chip8/Screen.java
+++ src/main/java/chip8/Screen.java
@@ -1,12 +1,16 @@
 package chip8;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Miguel Moreno
  * This class implements the screen
  */
 public class Screen {
+
+    //A lock is needed to avoid printing the screen while a sprite is being drawn onto it
+    ReentrantLock lock = new ReentrantLock(true);
 
     //Dimensions of the screen
     public static final short SCREEN_HEIGHT = 32;
@@ -20,7 +24,10 @@ public class Screen {
     private boolean[][] screenArray;
 
     //True if the screen has been modified since last time it was shown
-    public boolean drawFlag;
+    private boolean drawFlag;
+
+    //True if coordinates out of bounds are allowed to be used to draw sprites
+    private boolean outOfBoundsCoordinates;
 
     /**
      * Creates a blank screen
@@ -29,14 +36,15 @@ public class Screen {
         SCREEN_WRAP = true;
         screenArray = new boolean[SCREEN_WIDTH][SCREEN_HEIGHT];
         drawFlag = true;
+        outOfBoundsCoordinates = true;
     }
 
     Screen(Screen s) {
         SCREEN_WRAP = s.SCREEN_WRAP;
+        outOfBoundsCoordinates = s.outOfBoundsCoordinates;
         screenArray = new boolean[SCREEN_WIDTH][SCREEN_HEIGHT];
-        boolean[][] screen2 = s.getScreenArray();
         for (int i = 0; i< screenArray.length; i++) {
-            screenArray[i] = Arrays.copyOf(screen2[i], screen2[i].length);
+            screenArray[i] = Arrays.copyOf(s.screenArray[i], s.screenArray[i].length);
         }
         drawFlag = s.drawFlag;
     }
@@ -45,20 +53,44 @@ public class Screen {
      * Clears the screen by settings all pixels to false
      */
     public void clear() {
-        for (short i = 0; i< screenArray.length; i++) {
-            for (short j = 0; j< screenArray[i].length; j++) {
-                screenArray[i][j] = false;
+        lock.lock();
+        try {
+            for (short i = 0; i< screenArray.length; i++) {
+                for (short j = 0; j< screenArray[i].length; j++) {
+                    screenArray[i][j] = false;
+                }
             }
+            drawFlag = true;
+        } finally {
+            lock.unlock();
         }
-        drawFlag = true;
     }
 
     /**
-     * get the screen boolean array
+     * Get a copy of the screen boolean array
+     * @param setFlagToFalse if true then drawFlag will be set to false
+     */
+    public boolean[][] getScreenArray(boolean setFlagToFalse) {
+        lock.lock();
+        try {
+            boolean[][] screenCopy = new boolean[SCREEN_WIDTH][SCREEN_HEIGHT];
+            for (int i = 0; i< screenCopy.length; i++) {
+                screenCopy[i] = Arrays.copyOf(screenArray[i], screenArray[i].length);
+            }
+            if (setFlagToFalse) drawFlag = false;
+            return screenCopy;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Get a copy of the screen boolean array
      */
     public boolean[][] getScreenArray() {
-        return screenArray;
+        return getScreenArray(false);
     }
+
 
     /**
      * Draw a sprite in the screen
@@ -69,36 +101,49 @@ public class Screen {
      */
     public boolean drawSprite(byte[] sprite, short x, short y) {
 
-        //Check if coordinates are out of bounds
-        if (x<0 || x>= SCREEN_WIDTH || y<0 || y>= SCREEN_HEIGHT) {
-            System.err.println("Sprite coordinates out of bounds");
-            return false;
-        }
+        lock.lock();
+        try {
 
-        //Set to true if an on pixel has been overwritten to off
-        boolean pixelOverwrite = false;
-
-        for (byte line : sprite) {
-            byte mask = 1;
-            for (byte i = 7; i>=0; i--) {
-                //Avoid wrapping if wrap is set to false
-                if (!(x+i>= SCREEN_WIDTH && !SCREEN_WRAP)) {
-                    //Calculate the pixel
-                    boolean pixel = (line & mask) == mask;
-                    //Check if an on pixel will be overwritten to off
-                    if (pixel==true && screenArray[(x+i)% SCREEN_WIDTH][y% SCREEN_HEIGHT]==true) pixelOverwrite=true;
-                    //XOR the pixel
-                    screenArray[(x+i)% SCREEN_WIDTH][y% SCREEN_HEIGHT] ^= pixel;
+            //Check if coordinates are out of bounds
+            if (x<0 || x>= SCREEN_WIDTH || y<0 || y>= SCREEN_HEIGHT) {
+                System.err.println("Sprite coordinates out of bounds");
+                if (!outOfBoundsCoordinates) {
+                    return false;
                 }
-                mask <<= 1;
+                else {
+                    x %= SCREEN_WIDTH;
+                    y %= SCREEN_HEIGHT;
+                }
             }
-            y++;
-            if (y>= SCREEN_HEIGHT && !SCREEN_WRAP) break;
+
+            //Set to true if an on pixel has been overwritten to off
+            boolean pixelOverwrite = false;
+
+            for (byte line : sprite) {
+                byte mask = 1;
+                for (byte i = 7; i>=0; i--) {
+                    //Avoid wrapping if wrap is set to false
+                    if (!(x+i>= SCREEN_WIDTH && !SCREEN_WRAP)) {
+                        //Calculate the pixel
+                        boolean pixel = (line & mask) == mask;
+                        //Check if an on pixel will be overwritten to off
+                        if (pixel==true && screenArray[(x+i)% SCREEN_WIDTH][y% SCREEN_HEIGHT]==true) pixelOverwrite=true;
+                        //XOR the pixel
+                        screenArray[(x+i)%SCREEN_WIDTH][y%SCREEN_HEIGHT] ^= pixel;
+                    }
+                    mask <<= 1;
+                }
+                y++;
+                if (y>= SCREEN_HEIGHT && !SCREEN_WRAP) break;
+            }
+
+            drawFlag = true;
+
+            return pixelOverwrite;
+
+        } finally {
+            lock.unlock();
         }
-
-        drawFlag = true;
-
-        return pixelOverwrite;
     }
 
     @Override
@@ -116,6 +161,10 @@ public class Screen {
             s += "\n";
         }
         return s;
+    }
+
+    public boolean getDrawFlag() {
+        return drawFlag;
     }
 
 }
